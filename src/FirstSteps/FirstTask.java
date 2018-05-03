c8dc251cdf1a9a651de6d0a3166bc2b0750db14f
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-03 12:29:24 +0200

    First step into Java

---
+++ src/FirstSteps/FirstTask.java
@@ -0,0 +1,9 @@
+package FirstSteps;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("Ania");
+        System.out.println("Bartek");
+        System.out.println("Kasia");
+    }
+}
