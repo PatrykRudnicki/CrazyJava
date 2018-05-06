4655a0b8461c041bd406c4dc63dbab30a682857a
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-06 17:20:32 +0200

    Fifth task

--- src/FirstSteps/FifthTask.java
+++ src/FirstSteps/FifthTask.java
@@ -1,4 +1,16 @@
 package FirstSteps;
 
+import java.math.BigInteger;
+
 public class FifthTask {
+    public static void main(String[] args){
+        BigInteger a = new BigInteger("-123456789123456789");
+        BigInteger b = new BigInteger("987654321987654321");
+
+        System.out.println(a.add(b));
+        System.out.println(b.subtract(a));
+        System.out.println(a.multiply(b));
+        System.out.println(a.pow(2));
+        System.out.println(a.pow(3));
+    }
 }
