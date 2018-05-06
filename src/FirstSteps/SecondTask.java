19f490db7496f656710407f1472b0ae5463e7fdf
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-06 16:37:42 +0200

    Second task

--- src/FirstSteps/SecondTask.java
+++ src/FirstSteps/SecondTask.java
@@ -1,4 +1,15 @@
 package FirstSteps;
 
 public class SecondTask {
+    public static void main(String[] args){
+        String string1 = "First";
+        String string2 = "Second";
+        String string3 = "Third";
+        String string4 = "Fourth";
+
+        int lengthFirstTwoWords = (string1 + string2).length();
+        String everything = string1 + string2 + string3 + string4;
+
+        System.out.println(everything.substring(0, lengthFirstTwoWords));
+    }
 }
--- src/FirstSteps/Variables.java
+++ src/FirstSteps/Variables.java
@@ -1,4 +1,23 @@
 package FirstSteps;
 
 public class Variables {
+    public static void main(String[] args){
+        int number;
+        number = 5;
+
+        char character = 'A';
+
+        int number1 = 10;
+        int number2 = number1 + 20;
+
+        System.out.println(number1 + number2);
+
+        final double Pi = 3.14;
+
+        String Welcome = "Hello World!";
+
+        String hello = Welcome.substring(0,6);
+
+        System.out.println(hello);
+    }
 }
