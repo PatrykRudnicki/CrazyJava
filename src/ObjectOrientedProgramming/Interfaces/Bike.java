6a977d3a07d58e5e145e82dc9f808298182a9f3c
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-10 21:53:59 +0200

    Interfaces

---
+++ src/ObjectOrientedProgramming/Interfaces/Bike.java
@@ -0,0 +1,11 @@
+package ObjectOrientedProgramming.Interfaces;
+
+public class Bike implements Vehicle {
+    @Override
+    public void drive(int speed){}
+
+    @Override
+    public void stop(){}
+
+    public void jump(){}
+}
---
+++ src/ObjectOrientedProgramming/Interfaces/Vehicle.java
@@ -0,0 +1,6 @@
+package ObjectOrientedProgramming.Interfaces;
+
+public interface Vehicle {
+    public void drive(int speed);
+    public void stop();
+}
